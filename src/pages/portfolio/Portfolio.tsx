b055eadf5e206d74c6300fe6829be8ed02bea6d8
Trang Truong <trangtruong.071105@gmail.com>
2026-03-30 23:36:45 -0700

    Show all projects by default

--- src/pages/portfolio/Portfolio.tsx
+++ src/pages/portfolio/Portfolio.tsx
@@ -16,7 +16,7 @@ import ProjectTimeline from "./ProjectTimeline";
 import OpenSourceBanner from "./OpenSourceBanner";
 
 const Portfolio = () => {
-   const [activeFilter, setActiveFilter] = useState<string>("Featured");
+   const [activeFilter, setActiveFilter] = useState<string>("All");
 
    const handleFilterChange = useCallback((filter: string) => {
       if (document.startViewTransition) {

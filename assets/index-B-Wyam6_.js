1eed5110214905736955642f9931237e67851b21
Trang Truong <trangtruong.071105@gmail.com>
2026-07-21 10:26:44 -0700

    Updates

---
+++ assets/index-B-Wyam6_.js
@@ -0,0 +1,1 @@
+import{r}from"./threejs-DHpKkmUb.js";var a=r();export{a as r};
---
+++ assets/use-in-view-sj1XZzxz.js
@@ -0,0 +1,1 @@
+import{r as f}from"./icons-DPeI8s9t.js";import{a7 as p}from"./animations-Dxkn1mQG.js";function V(e,{root:t,margin:n,amount:s,once:r=!1,initial:o=!1}={}){const[u,i]=f.useState(o);return f.useEffect(()=>{if(!e.current||r&&u)return;const a=()=>(i(!0),r?void 0:()=>i(!1)),c={root:t&&t.current||void 0,margin:n,amount:s};return p(e.current,a,c)},[t,e,n,r,s]),u}export{V as u};
